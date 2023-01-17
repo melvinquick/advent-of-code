a0aa9881def815584daa1a836a892e6b76c17d66
Charlie Quick <cquickreal00@gmail.com>
2023-01-16 20:49:37 -0500

    Update rock_paper_scissors.py
    Combined both parts for day 2 into one .py for simplicity.

--- 2022/day-2/rock_paper_scissors.py
+++ 2022/day-2/rock_paper_scissors.py
@@ -17,7 +17,7 @@ def get_strategy_inputs_func():
     return strategy_inputs
 
 
-def get_total_score_func(local_strategy_array):
+def part_1_score_func(local_strategy_array):
     # This function takes the previously read in strategy file and calculates your total score
     score_sum = 0
     strategies = local_strategy_array
@@ -44,7 +44,35 @@ def get_total_score_func(local_strategy_array):
                 score_sum += 6
             case _:
                 print("Not a valid outcome")
+    return score_sum
 
+def part_2_score_func(local_strategy_array):
+    # This function takes the previously read in strategy file and calculates your total score
+    score_sum = 0
+    strategies = local_strategy_array
+
+    for strategy in strategies:
+        match strategy:
+            case "A X":
+                score_sum += 3
+            case "A Y":
+                score_sum += 4
+            case "A Z":
+                score_sum += 8
+            case "B X":
+                score_sum += 1
+            case "B Y":
+                score_sum += 5
+            case "B Z":
+                score_sum += 9
+            case "C X":
+                score_sum += 2
+            case "C Y":
+                score_sum += 6
+            case "C Z":
+                score_sum += 7
+            case _:
+                print("Not a valid outcome")
     return score_sum
 
 
@@ -55,8 +83,12 @@ def main():
     total_score = 0
 
     strategy_array = get_strategy_inputs_func()
-    total_score = get_total_score_func(strategy_array)
-    print(total_score)
+
+    total_score = part_1_score_func(strategy_array)
+    print("Part 1: ", total_score)
+
+    total_score = part_2_score_func(strategy_array)
+    print("Part 2: ", total_score)
 
 if __name__ == "__main__":
     main()

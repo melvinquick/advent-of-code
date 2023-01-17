5b8def7efafe630c04f01f5feb1de76896ee7ece
Charlie Quick <cquickreal00@gmail.com>
2023-01-16 21:16:14 -0500

    Update camp-cleanup.py
    Combined both parts into one .py file.

--- 2022/day-4/camp-cleanup.py
+++ 2022/day-4/camp-cleanup.py
@@ -46,18 +46,36 @@ def get_enveloped_count_func(passed_split_pairs):
         counter += 1
     return enveloped_pairs_counter
 
+def get_overlapped_count_func(passed_split_pairs):
+    # Function to check pairs are overlapping at all
+    overlapped_pairs_counter = 0
+    counter = 0
+    index_0 = 0
+    index_1 = 1
+
+    for pair in passed_split_pairs:
+        if (int(pair[index_0][index_0]) >= int(pair[index_1][index_0])) & (int(pair[index_0][index_0]) <= int(pair[index_1][index_1])):
+            overlapped_pairs_counter += 1
+        elif (int(pair[index_0][index_1]) >= int(pair[index_1][index_0])) & (int(pair[index_0][index_1]) <= int(pair[index_1][index_1])):
+            overlapped_pairs_counter += 1
+        elif (int(pair[index_1][index_0]) >= int(pair[index_0][index_0])) & (int(pair[index_1][index_0]) <= int(pair[index_0][index_1])):
+            overlapped_pairs_counter += 1
+        elif (int(pair[index_1][index_1]) >= int(pair[index_0][index_0])) & (int(pair[index_1][index_1]) <= int(pair[index_0][index_1])):
+            overlapped_pairs_counter += 1
+        counter += 1
+    return overlapped_pairs_counter
+
 
 # --- Main --- #
 
 def main():
-    pairs = []
-    split_pairs = []
-    enveloped_counter = 0
-
     pairs = get_pairs_func()
     split_pairs = split_pairs_func(pairs)
     enveloped_counter = get_enveloped_count_func(split_pairs)
+    overlapped_counter = get_overlapped_count_func(split_pairs)
+
     print("Part 1: ", enveloped_counter)
+    print("Part 2: ", overlapped_counter)
 
 if __name__ == "__main__":
     main()

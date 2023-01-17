2e17b9dde91d13730d368118dfa6b6077ebc63e1
Charlie Quick <cquickreal00@gmail.com>
2023-01-16 21:41:58 -0500

    Update supply-stacks.py
    Combined parts 1 and 2 together into one .py

--- 2022/day-5/supply-stacks.py
+++ 2022/day-5/supply-stacks.py
@@ -5,15 +5,17 @@ import sys
 
 # --- Functions --- #
 
+
 def get_crates_and_moves_func():
     # Function to get the crate info that the elves currently have
     crates_and_moves = []
     crates_file = open(os.path.join(sys.path[0], "input.txt"), "r")
 
     for crate in crates_file:
-        crates_and_moves.append(crate.replace("\n",""))
+        crates_and_moves.append(crate.replace("\n", ""))
     crates_file.close()
     return crates_and_moves
+
 
 def get_moves_func(passed_crates_and_moves):
     # Function to get just the moves from the input
@@ -24,8 +26,10 @@ def get_moves_func(passed_crates_and_moves):
         if "move" in line:
             moves_1.append(line)
     for line in moves_1:
-        moves_2.append(line.strip("move ").replace("from ","").replace("to ","").split())
+        moves_2.append(line.strip("move ").replace(
+            "from ", "").replace("to ", "").split())
     return moves_2
+
 
 def get_crates_func(passed_crates_and_moves):
     # Function to get just the crates from the input
@@ -38,6 +42,7 @@ def get_crates_func(passed_crates_and_moves):
             crates.append(line)
     crates.pop()
     return crates
+
 
 def remove_extra_crate_chars_func(passed_crates):
     # Function to remove extra characters from crates
@@ -53,6 +58,7 @@ def remove_extra_crate_chars_func(passed_crates):
                 chars.append(char)
         lines.append(chars)
     return lines
+
 
 def correct_crate_columns(passed_crates):
     # Function to get crates into correct columns
@@ -83,12 +89,8 @@ def correct_crate_columns(passed_crates):
         crates_5.append(chars)
     return crates_5
 
-def print_nicely_func(passed_crates_or_moves):
-    # Function to nicely print crate list
-    for line in passed_crates_or_moves:
-        print(line)
 
-def do_moves(passed_crates, passed_moves):
+def do_moves_part_1(passed_crates, passed_moves):
     # Function to do moves from input
     top_crates = []
     counter_1 = 0
@@ -97,8 +99,33 @@ def do_moves(passed_crates, passed_moves):
     for move in passed_moves:
         counter_1 = int(move[0])-1
         while counter_1 >= 0:
-            passed_crates[int(move[2])-1].append(passed_crates[int(move[1])-1].pop())
+            passed_crates[int(move[2]) -
+                          1].append(passed_crates[int(move[1])-1].pop())
             counter_1 -= 1
+    while counter_2 <= (len(passed_crates)-1):
+        top_crates.append(passed_crates[counter_2][-1])
+        counter_2 += 1
+    return top_crates
+
+
+def do_moves_part_2(passed_crates, passed_moves):
+    # Function to do moves from input but for part 2
+    top_crates = []
+    temp_list = []
+    temp_counter = 0
+    counter_1 = 0
+    counter_2 = 0
+
+    for move in passed_moves:
+        counter_1 = int(move[0])-1
+        temp_list = []
+        while counter_1 >= 0:
+            temp_list.append(passed_crates[int(move[1])-1].pop())
+            counter_1 -= 1
+        temp_counter = len(temp_list)-1
+        while temp_counter >= 0:
+            passed_crates[int(move[2])-1].append(temp_list.pop())
+            temp_counter -= 1
     while counter_2 <= (len(passed_crates)-1):
         top_crates.append(passed_crates[counter_2][-1])
         counter_2 += 1
@@ -113,8 +140,18 @@ def main():
     crates = get_crates_func(crates_and_moves)
     crates = remove_extra_crate_chars_func(crates)
     crates = correct_crate_columns(crates)
-    top_crates = do_moves(crates, moves)
-    print_nicely_func(top_crates)
+    top_crates = do_moves_part_1(crates, moves)
+
+    print("Part 1: ", top_crates)
+
+    crates_and_moves = get_crates_and_moves_func()
+    moves = get_moves_func(crates_and_moves)
+    crates = get_crates_func(crates_and_moves)
+    crates = remove_extra_crate_chars_func(crates)
+    crates = correct_crate_columns(crates)
+    top_crates = do_moves_part_2(crates, moves)
+
+    print("Part 2: ", top_crates)
 
 
 if __name__ == "__main__":
